ed30e339f499dedb9440b7cdb439ea9518a0bc53
Quanxibanyang <109034199+Quanxibanyang@users.noreply.github.com>
2022-07-30 18:26:02 +0100

    Update tf_planck2018_lite_posterior.py

--- cosmopower/likelihoods/tf_planck2018_lite/tf_planck2018_lite_posterior.py
+++ cosmopower/likelihoods/tf_planck2018_lite/tf_planck2018_lite_posterior.py
@@ -319,7 +319,7 @@ class tf_planck2018_lite_posterior:
         
         
         diff = tf.subtract(delta_p, tf.transpose(delta_bar))
-        Sig_pro = tf.matmul(tf.transpose(diff),diff)
+        Sig_pro = tf.matmul(diff,tf.transpose(diff))
 
         #Sig_pro = tf.matmul(tf.subtract(delta_p, delta_bar),
          #                   tf.transpose(tf.subtract(delta_p, delta_bar)))
