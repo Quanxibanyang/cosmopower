eec25c300f4c4ef6424db4fa9e50d2ea9b97e563
Quanxibanyang <109034199+Quanxibanyang@users.noreply.github.com>
2022-07-31 00:07:43 +0100

    Update tf_planck2018_lite_posterior.py

--- cosmopower/likelihoods/tf_planck2018_lite/tf_planck2018_lite_posterior.py
+++ cosmopower/likelihoods/tf_planck2018_lite/tf_planck2018_lite_posterior.py
@@ -320,7 +320,7 @@ class tf_planck2018_lite_posterior:
         #Here only need those parameters data
         
         differ = tf.subtract(delta_p, tf.transpose(delta_bar))
-        Sig_pro = tf.matmul(diff,tf.transpose(diff))
+        Sig_pro = tf.matmul(differ,tf.transpose(differ))
         
         #the matrix product inside the summation of Sigma
         
